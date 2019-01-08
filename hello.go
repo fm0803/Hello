387b5801a7688a65b85422429a6d16e55ca31043
fm0803 <1325039961@qq.com>
2019-01-08 07:17:14 +0000

    update patterns

--- hello.go
+++ hello.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/fm0803/stringutil"
+)
 
 func main() {
-		fmt.Println("Hello, world.")
-	}
-
+		fmt.Println(stringutil.Reverse("!oG ,olleH"))
+}
