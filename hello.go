53f370761646cbb4974b600ee2de9b33f2bd5c28
fangman <fangman@sensetime.com>
2020-11-04 10:11:29 +0800

    string to bytes

--- hello.go
+++ hello.go
@@ -6,6 +6,14 @@ import (
 )
 
 func main() {
-		fmt.Println("!oG ,olleH")
-		//fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println("!oG ,olleH")
+	//fmt.Println(stringutil.Reverse("!oG ,olleH"))
+
+	str2 := "hello"
+	data2 := []byte(str2)
+	fmt.Println(data2)
+	str2 = string(data2[:])
+	fmt.Println(str2)
+	data2 = nil
+	fmt.Println(data2)
 }
